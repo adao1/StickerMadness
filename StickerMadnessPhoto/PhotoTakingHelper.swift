b2d855e1859b919c7464fc24f87faa00b22a43f4
Alexander Dao <adao@berkeley.edu>
2016-06-29 07:30:47 -0700

    basic functions working

---
+++ StickerMadnessPhoto/PhotoTakingHelper.swift
@@ -0,0 +1,77 @@
+//
+//  PhotoTakingHelper.swift
+//  StickerMadnessPhoto
+//
+//  Created by Alex Dao on 6/28/16.
+//  Copyright © 2016 Alex Dao. All rights reserved.
+//
+
+import UIKit
+
+typealias PhotoTakingHelperCallback = UIImage? -> Void
+
+class PhotoTakingHelper : NSObject {
+    
+    // View controller on which AlertViewController and UIImagePickerController are presented
+    weak var viewController: UIViewController!
+    var callback: PhotoTakingHelperCallback
+    var imagePickerController: UIImagePickerController?
+    
+    init(viewController: UIViewController, callback: PhotoTakingHelperCallback) {
+        self.viewController = viewController
+        self.callback = callback
+        
+        super.init()
+        
+        showPhotoSourceSelection()
+    }
+    
+    func showPhotoSourceSelection() {
+        // Allow user to choose between photo library and camera
+        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo from Library", style: .Default) { (action) in
+            self.showImagePickerController(.PhotoLibrary)
+        }
+        
+        alertController.addAction(photoLibraryAction)
+        
+        
+        // Only show camera option if rear camera is available
+        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
+            let cameraAction = UIAlertAction(title: "Photo from Camera", style: .Default) { (action) in
+                self.showImagePickerController(.Camera)
+            }
+            
+            alertController.addAction(cameraAction)
+        }
+        
+        viewController.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController!.sourceType = sourceType
+        imagePickerController!.delegate = self
+        
+        self.viewController.presentViewController(imagePickerController!, animated: true, completion: nil)
+    }
+    
+}
+
+extension PhotoTakingHelper: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        viewController.dismissViewControllerAnimated(false, completion: nil)
+        
+        callback(image)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
--- StickerMadnessPhoto/ViewController.swift
+++ StickerMadnessPhoto/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var photoTakingHelper: PhotoTakingHelper?
 
+    @IBOutlet weak var displayedImage: UIImageView!
+    @IBOutlet weak var hatSticker: UIImageView!
+    @IBOutlet weak var mustacheSticker: UIImageView!
+    @IBOutlet weak var glassesSticker: UIImageView!
+    var hat = false
+    var mustache = false
+    var glasses = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tabBarController?.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            print("received a callback")
+            self.displayedImage.image = image
+        }
     }
+    
+    @IBAction func hatAction(sender: AnyObject) {
+        if !hat {
+            hatSticker.image = UIImage(named: "hat1")
+            hat = true
+        } else {
+            hat = false
+            hatSticker.image = nil
+        }
+    }
+    
+    @IBAction func mustacheAction(sender: AnyObject) {
+        if !mustache {
+            mustacheSticker.image = UIImage(named: "mustache1")
+            mustache = true
+        } else {
+            mustache = false
+            mustacheSticker.image = nil
+        }
 
+    }
+    @IBAction func glassesAction(sender: AnyObject) {
+        if !glasses {
+            glassesSticker.image = UIImage(named: "eyeglasses1")
+            glasses = true
+        } else {
+            glasses = false
+            glassesSticker.image = nil
+        }
 
+    }
 }
 
+// MARK: Tab Bar Delegate
+
+extension ViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is ViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+}
